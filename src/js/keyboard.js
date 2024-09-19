05be4a98e36a031da2e4ed94a8c309f3c05d4876
mouton <45201897+ISheepFr@users.noreply.github.com>
2024-09-19 18:34:23 +0200

    Update keyboard.js

--- src/js/keyboard.js
+++ src/js/keyboard.js
@@ -1,4 +1,4 @@
-import * as THREE from 'https://cdn.skypack.dev/three@0.128.0/build/three.module';
+import * as THREE from 'https://unpkg.com/three@0.127.0/build/three.module.js';
 import * as GLTFLoader from 'https://unpkg.com/three@0.126.0/examples/js/loaders/GLTFLoader.js';
 import * as OrbitControls from 'https://cdn.jsdelivr.net/npm/three-orbitcontrols@2.110.3/OrbitControls.min.js';
 
