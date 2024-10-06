a84a42a1a9d47001b9c1e4fa5a6b1a0c23563e79
antoine <45201897+ISheepFr@users.noreply.github.com>
2024-10-06 17:26:59 +0200

    aruco

--- src/js/home_page.js
+++ src/js/home_page.js
@@ -1,3 +1,8 @@
 document.getElementById('home_page').addEventListener('click', function() {
             window.location.href = '../../index.html';  
 });
+
+document.getElementById('projects_page').addEventListener('click', function() {
+    window.location.href = '../../recap.html';  
+});
+
