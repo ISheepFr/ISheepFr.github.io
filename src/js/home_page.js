f32f9d02db6098322a9972a0ada95acdb97ebce9
antoine <45201897+ISheepFr@users.noreply.github.com>
2024-10-14 20:23:29 +0200

    fix card size

--- src/js/home_page.js
+++ src/js/home_page.js
@@ -2,7 +2,31 @@ document.getElementById('home_page').addEventListener('click', function() {
             window.location.href = '../../index.html';  
 });
 
-document.getElementById('projects_page').addEventListener('click', function() {
+if(document.getElementById('projects_page') != null)
+{
+    document.getElementById('projects_page').addEventListener('click', function() {
     window.location.href = '../../recap.html';  
-});
+    });
+}
+
+
+const dropdownButton = document.getElementById('dropdownMenuButton');
+const cardsSection = document.getElementById('cont_cards');
+
+if(dropdownButton != null && cardsSection != null)
+{
+   // Ajouter un événement pour détecter lorsque la dropdown s'ouvre
+dropdownButton.addEventListener('click', function () {
+    const dropdownMenu = document.querySelector('.dropdown-menu');
+    console.log(dropdownMenu.classList.contains('show'));
+    if (dropdownMenu.classList.contains('show')) {
+        cardsSection.style.marginTop = '120px';
+    } else {
+        // Ajouter de l'espace lorsque le menu est ouvert
+        cardsSection.style.marginTop = '0px';  // Ajustez cette valeur selon vos besoins
+    }
+}); 
+}
+
+
 
