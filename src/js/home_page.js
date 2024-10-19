b774d7963ab2ad0f648007f2e56e437190195a60
antoine <45201897+ISheepFr@users.noreply.github.com>
2024-10-19 18:42:28 +0200

    clear

--- src/js/home_page.js
+++ src/js/home_page.js
@@ -5,27 +5,11 @@ document.getElementById('home_page').addEventListener('click', function() {
 if(document.getElementById('projects_page') != null)
 {
     document.getElementById('projects_page').addEventListener('click', function() {
-    window.location.href = '../../recap.html';  
+
+    let currentPath = window.location.pathname;
+    let pageName = currentPath.split("/").pop().split(".")[0];
+    window.location.href = `../../recap.html#${pageName}`;
     });
-}
-
-
-const dropdownButton = document.getElementById('dropdownMenuButton');
-const cardsSection = document.getElementById('cont_cards');
-
-if(dropdownButton != null && cardsSection != null)
-{
-   // Ajouter un événement pour détecter lorsque la dropdown s'ouvre
-dropdownButton.addEventListener('click', function () {
-    const dropdownMenu = document.querySelector('.dropdown-menu');
-    console.log(dropdownMenu.classList.contains('show'));
-    if (dropdownMenu.classList.contains('show')) {
-        cardsSection.style.marginTop = '120px';
-    } else {
-        // Ajouter de l'espace lorsque le menu est ouvert
-        cardsSection.style.marginTop = '0px';  // Ajustez cette valeur selon vos besoins
-    }
-}); 
 }
 
 
